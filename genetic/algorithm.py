533dceb6815de3a8bbc0ecaa25cd54d54d7f535a
John Mikhailov <mikhailovj@gmail.com>
2019-05-13 16:53:28 +0300

    mutate function was added

--- genetic/algorithm.py
+++ genetic/algorithm.py
@@ -1,8 +1,5 @@
 from random import randint as rnd
 from PIL import Image, ImageDraw
-import numpy as np
-import matplotlib.path as mp
-from collections import defaultdict
 from scipy.spatial import Delaunay
 from genetic.coloring import color
 from genetic.fitness import fitness
@@ -10,7 +7,7 @@ from genetic.fitness import fitness
 
 class Algorithm:
 
-    def __init__(self, input_path, output_path, points_amount, steps=500):
+    def __init__(self, input_path, output_path='', points_amount=1000, steps=500):
         self.image_in = Image.open(input_path)  # input image
         self.image_out = Image.open(input_path)  # image that will be processed
         self.pixels = self.image_in.load()
@@ -18,28 +15,39 @@ class Algorithm:
         self.layer = Image.new('RGBA', (self.width, self.height))
         self.draw = ImageDraw.Draw(self.layer)
         self.points_amount = points_amount
+        self.points = []
         self.steps = steps
 
     def start(self):
-        points = self.generate_points()
+        self.generate_points()
         for i in range(self.steps):
-            self.draw_triangles(points)
-            print(fitness(self.image_in, self.image_out))
-            self.mutate()
+            self.draw_triangles()
+            print(fitness(self.image_in.getdata(), self.image_out.getdata()))
+            # self.mutate()
 
     def generate_points(self):
-        return [[0, 0], [0, self.height], [self.width, 0], [self.width, self.height]] +\
+        self.points = [[0, 0], [0, self.height], [self.width, 0], [self.width, self.height]] +\
                [[rnd(0, self.width), rnd(0, self.height)] for _ in range(self.points_amount)]
 
     def mutate(self):
-        pass
+        for point in self.points:
+            delta_x = rnd(-2, 2)
+            delta_y = rnd(-2, 2)
+            if point[0] + delta_x > self.width - 1:
+                point[0] -= delta_x
+            else:
+                point[1] += delta_x
+            if point[1] + delta_y > self.height - 1:
+                point[1] -= delta_y
+            else:
+                point[1] += delta_y
 
-    def draw_triangles(self, points):
-        tri = Delaunay(points)
+    def draw_triangles(self):
+        tri = Delaunay(self.points)
         for i in tri.simplices:
-            a = tuple(points[i[0]])
-            b = tuple(points[i[1]])
-            c = tuple(points[i[2]])
+            a = tuple(self.points[i[0]])
+            b = tuple(self.points[i[1]])
+            c = tuple(self.points[i[2]])
             fill = color([a, b, c], self.pixels)
             self.draw.polygon([a, b, c], fill=fill)
         self.image_out.paste(self.layer, mask=self.layer)
