084e7cb4290c37e3fa91afd616536ed2db59345c
John Mikhailov <mikhailovj@gmail.com>
2019-05-14 23:43:49 +0300

    mutate function deleted

--- genetic/algorithm.py
+++ genetic/algorithm.py
@@ -7,7 +7,7 @@ from genetic.fitness import fitness
 
 class Algorithm:
 
-    def __init__(self, input_path, output_path='', points_amount=1000, steps=500):
+    def __init__(self, input_path, points_amount=1000, steps=500):
         self.image_in = Image.open(input_path)  # input image
         self.image_out = Image.open(input_path)  # image that will be processed
         self.pixels = self.image_in.load()
@@ -22,25 +22,10 @@ class Algorithm:
         self.generate_points()
         for i in range(self.steps):
             self.draw_triangles()
-            print(fitness(self.image_in.getdata(), self.image_out.getdata()))
-            # self.mutate()
 
     def generate_points(self):
         self.points = [[0, 0], [0, self.height], [self.width, 0], [self.width, self.height]] +\
                [[rnd(0, self.width), rnd(0, self.height)] for _ in range(self.points_amount)]
-
-    def mutate(self):
-        for point in self.points:
-            delta_x = rnd(-2, 2)
-            delta_y = rnd(-2, 2)
-            if point[0] + delta_x > self.width - 1:
-                point[0] -= delta_x
-            else:
-                point[1] += delta_x
-            if point[1] + delta_y > self.height - 1:
-                point[1] -= delta_y
-            else:
-                point[1] += delta_y
 
     def draw_triangles(self):
         tri = Delaunay(self.points)
