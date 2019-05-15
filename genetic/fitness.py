55f8a26b51a254b7aa3418cd3946fe84bbfc26d6
John Mikhailov <mikhailovj@gmail.com>
2019-05-15 16:05:44 +0300

    fitness function renamed

--- genetic/fitness.py
+++ genetic/fitness.py
@@ -8,7 +8,7 @@ def fitness(original, selected):
     return fit
 
 
-def fitness_(original, selected, points):
+def fitness_selected(original, selected, points):
     fit = 0
     for i in range(len(points)):
         r = original[points[i][0]][points[i][1]][0] - selected[points[i][0]][points[i][1]][0]
